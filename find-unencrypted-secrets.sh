b2eb7bc1d46b682b667bbc188ff5b5ea511fabff
anthr76 <hello@anthonyrabbito.com>
2021-04-12 15:37:05 -0400

    Initial commit
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

---
+++ find-unencrypted-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## angelnu k8s@home
+FILE_WITH_SECRETS=$(grep -lZRPi '^kind:\s+secret' . | xargs -r0 grep -L 'ENC.AES256')
+if [ -z "$FILE_WITH_SECRETS" ]; then
+  exit 0
+else
+  echo "$FILE_WITH_SECRETS has unencrypted secrets! (or at least not encrypted with AES256)"
+  exit 1
+fi
