53aaaeff04182f4c0574bd07a49474f72efdd2a4
anthr76 <hello@anthonyrabbito.com>
2021-04-13 09:38:04 -0400

    Fix up script revise regex
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- find-unencrypted-secrets.sh
+++ find-unencrypted-secrets.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+
+# OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
+# especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
+# workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
+export PATH=$PATH:/usr/local/bin
 
 secret_regex="^kind:\s+secret"
 sops_regex="ENC.AES256"
-
 
 has_error=0
 for file in "$@"; do
