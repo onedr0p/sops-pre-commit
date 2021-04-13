0bedeafea7454e485a03604dd9cfd23069a07755
anthr76 <hello@anthonyrabbito.com>
2021-04-13 08:33:53 -0400

    Add tests directory try wiring in CI
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- find-unencrypted-secrets.sh
+++ find-unencrypted-secrets.sh
@@ -1,9 +1,45 @@
-#!/bin/sh
-## angelnu k8s@home
-FILE_WITH_SECRETS=$(grep -lZRPi '^kind:\s+secret' . | xargs -r0 grep -L 'ENC.AES256')
-if [ -z "$FILE_WITH_SECRETS" ]; then
-  exit 0
-else
-  echo "$FILE_WITH_SECRETS has unencrypted secrets! (or at least not encrypted with AES256)"
-  exit 1
-fi
+#!/usr/bin/env bash
+
+set -e 
+
+# OSX GUI apps do not pick up environment variables the same way as Terminal apps and there are no easy solutions,
+# especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
+# workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
+export PATH=$PATH:/usr/local/bin
+
+exit_status=1
+
+parse_arguments() {
+	while (($# > 0)); do
+		# Grab param and value splitting on " " or "=" with parameter expansion
+		local PARAMETER="${1%[ =]*}"
+		local VALUE="${1#*[ =]}"
+		if [[ "$PARAMETER" == "$VALUE" ]]; then VALUE="$2"; fi
+		shift
+		case "$PARAMETER" in
+		-*)
+			echo "Error: Unknown option: $PARAMETER" >&2
+			exit 1
+			;;
+		*)
+			files="$files $PARAMETER"
+			;;
+		esac
+	done
+}
+
+parse_arguments "$@"
+
+for FILE in $files; do
+  SECRET_REGEX='^kind:\s+secret'
+  SOPS_REGEX='ENC.AES256'
+  echo "$@"
+	if (grep -Pi "$SECRET_REGEX" "$FILE" >/dev/null); then
+		if ! (grep -P "$SOPS_REGEX" "$FILE" >/dev/null); then
+			exit_status=1
+      echo "$FILE: has unencrypted secrets! (or at least not encrypted with AES256)"
+		fi
+	fi
+done
+
+exit $exit_status
